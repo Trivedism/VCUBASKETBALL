36760c8fdf3ce531a435390c915d803ab2ac0a4d
EswarVeerapaneni <46966138+EswarVeerapaneni@users.noreply.github.com>
2020-05-03 22:23:52 -0400

    Update 508Project.php

--- 508Project.php
+++ 508Project.php
@@ -6,18 +6,18 @@ global $db;
     define("DB_DATABASE", "MySQL");
 
     $db = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+    echo "Here are your player Options, Select one ";
 ?>
 <select name = "players">
 <?php
 $sql = "SELECT * FROM players;";
 		$result = mysqli_query($db, $sql);
 		$resultCheck = mysqli_num_rows($result);
-		if($resultCheck>0) {
 			while($row = mysqli_fetch_assoc($result)) {
-			$player_names = $row[full_name];
+			$player_names = $row[first_name];
+			$player_heights = $row[last_name];
 			echo "player names are: ";
-			echo "<option value = '$player_names'> $player_names</option>";
-}
+			echo "<option value = '$player_names'> $player_names $player_heights</option>";
 }
 ?>
 </select>
@@ -27,5 +27,20 @@ $sql = "SELECT * FROM players;";
 	<title></title>
 </head>
 <body>
+<form action = "phpB.php" method = "get">
+		Enter Players First Name: <input type = "text" name = "name">
+		<input type = "submit">
+		</form>
+		<?php
+		$sql = "SELECT * FROM players WHERE first_name = '".$_GET["name"]."';";
+		$result = mysqli_query($db, $sql);
+		$resultCheck = mysqli_num_rows($result);
+		if($resultCheck>0) {
+		while($row = mysqli_fetch_assoc($result)) {
+			$player_heights = $row["height"];
+			echo "The Height of the player is ".$row["height"];
+			}
+			}
+		?>
 </body>
 </html>
